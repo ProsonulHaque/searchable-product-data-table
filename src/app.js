22461aaa218c32d221cfddc57d9ae284bd22122d
Prosonul Haque <proshonulhaq@gmail.com>
2025-05-30 09:47:54 +0600

    implemented static version

--- src/app.js
+++ src/app.js
@@ -1,8 +1,70 @@
 export default function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-      <p>This is a simple React application.</p>
+    <FilterableProductTable/>
+  );
+}
+
+function FilterableProductTable(){
+  return(
+    <div> 
+      <SearchBar/>
+      <br />
+      <ProductTable/>
     </div>
+  );
+}
+
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" placeholder="Search..." />
+      <br/>
+      <label>
+        <input type="checkbox" />
+        Only show products in stock
+      </label>
+    </div>
+  )
+}
+
+function ProductTable() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <br />
+        <tbody>
+          <ProductCateGoryRow category="Sporting Goods" />
+          <ProductRow name="Football" price="$49.99" />
+          <ProductRow name="Baseball" price="$9.99" />
+          <br />
+          <ProductCateGoryRow category="Electronics" />
+          <ProductRow name="iPod Touch" price="$99.99" />
+          <ProductRow name="iPhone 5" price="$399.99" />
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function ProductCateGoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
+  );
+}
+
+function ProductRow({ name, price }) {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{price}</td>
+    </tr>
   );
 }
